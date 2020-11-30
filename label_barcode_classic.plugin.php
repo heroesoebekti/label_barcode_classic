fd2a449c5b546f64e31ac08997a085acb4a51485
Heru Subekti <heroe_soebekti@yahoo.co.id>
2020-11-30 21:28:33 +0700

    initial commit

---
+++ label_barcode_classic.plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Label Barcode Lawasan
+ * Plugin URI: /
+ * Description: Label Barcode Lawasan adalah plugin label barcode yang paling anda butuhkan
+ * Version: 0.0.1
+ * Author: Heru Subekti
+ * Author URI: https://www.facebook.com/heroe.soebekti
+ */
+
+// get plugin instance
+$plugin = \SLiMS\Plugins::getInstance();
+
+// registering menus
+$plugin->registerMenu('bibliography', 'Label Barcode Classic', __DIR__ . '/index.php');
